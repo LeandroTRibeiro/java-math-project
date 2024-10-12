335d2b18c19740ff3ea337ea6c2d710ef9ee4fdb
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-12 20:24:52 -0300

    feat: set minimum resize of window

--- src/graphic/Window.java
+++ src/graphic/Window.java
@@ -29,6 +29,7 @@ public class Window extends JFrame {
 
     public Window() {
         setSize(800, 600);
+        setMinimumSize(new Dimension(800, 600));
         setExtendedState(MAXIMIZED_BOTH);
         setTitle("Trabalho Programação Orientada a Objetos");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
