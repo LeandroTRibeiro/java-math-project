6b36781f4d269b76cb026d2efd0aedf871ade2a8
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-12 21:11:31 -0300

    style: change grid layout

--- src/components/template/RuleOfThreeCalculatorView.java
+++ src/components/template/RuleOfThreeCalculatorView.java
@@ -2,145 +2,192 @@ package components.template;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import components.ui.NumericTextField;
 
 public class RuleOfThreeCalculatorView extends JPanel {
-	
-	private JTextField _value1;
-	private JTextField _value2;
-	private JTextField _value3;
-	private JTextField _value4;
-	
-	private ITriFunction<Double, Double, Double, Double> _calculationFunction;
-	
-	public RuleOfThreeCalculatorView(
-			ITriFunction<Double, Double, Double, Double> calculationFunction
-	) {
-		this._calculationFunction = calculationFunction;
-		
-		JPanel innerPanel = new JPanel(new GridLayout(2, 2, 10, 10));
-		
-		JLabel label1 = new JLabel("a: ");
-		label1.setForeground(Color.RED);
-		_value1 = new NumericTextField();
+    
+    private JTextField _value1;
+    private JTextField _value2;
+    private JTextField _value3;
+    private JTextField _value4;
+    
+    private ITriFunction<Double, Double, Double, Double> _calculationFunction;
+    
+    public RuleOfThreeCalculatorView(
+            ITriFunction<Double, Double, Double, Double> calculationFunction
+    ) {
+        this._calculationFunction = calculationFunction;
+        
+        JPanel innerPanel = new JPanel(new GridBagLayout()); 
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new java.awt.Insets(10, 2, 10, 2); 
+        gbc.fill = GridBagConstraints.BOTH; 
+
+        JLabel label1 = new JLabel("a: ");
+        label1.setForeground(Color.RED);
+        label1.setHorizontalAlignment(SwingConstants.LEFT);
+
+        _value1 = new NumericTextField();
         Color lightRed = new Color(255, 185, 185);
-		_value1.setBackground(lightRed);
-		
-		JLabel label2 = new JLabel("= r1: ");
-		label2.setForeground(Color.GREEN);
-		_value2 = new NumericTextField();
-		Color lightGreen = new Color(204, 255, 185);
-		_value2.setBackground(lightGreen);
-		
-		JLabel label3 = new JLabel("b:");
-		label3.setForeground(Color.BLUE);
-		_value3 = new NumericTextField();
-		Color lightBlue = new Color(185, 211, 255);
-		_value3.setBackground(lightBlue);
-		
-		_value3.addKeyListener(new EnterKeyListener());
-		
-		JLabel label4 = new JLabel("= r2: ");
-		_value4 = new NumericTextField();
-		
-		innerPanel.add(label1);
-		innerPanel.add(_value1);
-		innerPanel.add(label2);
-		innerPanel.add(_value2);
-		innerPanel.add(label3);
-		innerPanel.add(_value3);
-		innerPanel.add(label4);
-		innerPanel.add(_value4);
-		
-		setLayout(new BorderLayout());
-		
-		setBorder(BorderFactory.createCompoundBorder(
-				BorderFactory.createTitledBorder("Regra de Três"),
-				new EmptyBorder(15, 15, 15, 15)
-		));
-		
-		add(innerPanel, BorderLayout.CENTER);
-	}
-	
-	private void calculationResult() {
-		
-		try {
-			_value4.setBackground(Color.WHITE);	
-			_value4.setForeground(Color.BLACK);
-			
-			double a = getValue1();
-			double b = getValue2();
-			double c = getValue3();
-			
-			double result = _calculationFunction.apply(a, b, c);
-			
-			_value4.setText(String.valueOf(result));
-			Color lightGreen = new Color(204, 255, 185);
-			_value4.setBackground(lightGreen);	
-			
-		} catch(NumberFormatException ex) {
-			
-			clearFields();
-			_value4.setText("ERROR: Invalid Value");
-			_value4.setForeground(Color.RED);
-	        Color lightRed = new Color(255, 185, 185);
-			_value4.setBackground(lightRed);
-			
-		}
-	}
-	
-	private class EnterKeyListener implements KeyListener {
+        _value1.setBackground(lightRed);
+        _value1.setPreferredSize(new Dimension(_value1.getPreferredSize().width, 40));
+        
+        JLabel label2 = new JLabel("= r1: ");
+        label2.setForeground(Color.GREEN);
+        label2.setHorizontalAlignment(SwingConstants.LEFT);
+        
+        _value2 = new NumericTextField();
+        Color lightGreen = new Color(204, 255, 185);
+        _value2.setBackground(lightGreen);
+        _value2.setPreferredSize(new Dimension(_value2.getPreferredSize().width, 40));
+        
+        JLabel label3 = new JLabel("b:");
+        label3.setForeground(Color.BLUE);
+        label3.setHorizontalAlignment(SwingConstants.LEFT);
+        
+        _value3 = new NumericTextField();
+        Color lightBlue = new Color(185, 211, 255);
+        _value3.setBackground(lightBlue);
+        _value3.setPreferredSize(new Dimension(_value3.getPreferredSize().width, 40));
+        _value3.addKeyListener(new EnterKeyListener());
+        
+        JLabel label4 = new JLabel("= r2: ");
+        label4.setHorizontalAlignment(SwingConstants.LEFT);
+        _value4 = new NumericTextField();
+                
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.1;  
+        innerPanel.add(label1, gbc); 
+        
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+        gbc.weightx = 2.0;  
+        innerPanel.add(_value1, gbc); 
+        
+        gbc.gridx = 3;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.1;  
+        innerPanel.add(label2, gbc);
+        
+        gbc.gridx = 4;
+        gbc.gridwidth = 2; 
+        gbc.weightx = 2.0;  
+        innerPanel.add(_value2, gbc); 
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.1;  
+        innerPanel.add(label3, gbc); 
+        
+        gbc.gridx = 1;
+        gbc.gridwidth = 2; 
+        gbc.weightx = 2.0;  
+        innerPanel.add(_value3, gbc); 
+        
+        gbc.gridx = 3;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0.1;  
+        innerPanel.add(label4, gbc); 
+        
+        gbc.gridx = 4;
+        gbc.gridwidth = 2; 
+        gbc.weightx = 2.0;  
+        innerPanel.add(_value4, gbc); 
+        
+        setLayout(new BorderLayout());
+        
+        setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder("Regra de Três"),
+                new EmptyBorder(0, 5, 0, 5)
+        ));
+        
+        add(innerPanel, BorderLayout.CENTER);
+    }
+    
+    private void calculationResult() {
+        
+        try {
+            _value4.setBackground(Color.WHITE);    
+            _value4.setForeground(Color.BLACK);
+            
+            double a = getValue1();
+            double b = getValue2();
+            double c = getValue3();
+            
+            double result = _calculationFunction.apply(a, b, c);
+            
+            _value4.setText(String.valueOf(result));
+            Color lightGreen = new Color(204, 255, 185);
+            _value4.setBackground(lightGreen);    
+            
+        } catch(NumberFormatException ex) {
+            
+            clearFields();
+            _value4.setText("ERROR: Invalid Value");
+            _value4.setForeground(Color.RED);
+            Color lightRed = new Color(255, 185, 185);
+            _value4.setBackground(lightRed);
+            
+        }
+    }
+    
+    private class EnterKeyListener implements KeyListener {
 
-		@Override
-		public void keyPressed(KeyEvent e) {
-			
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				calculationResult();
-			}			
-			
-		}
-		
-		@Override
-		public void keyTyped(KeyEvent e) {
-			// TODO Auto-generated method stub
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+            if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                calculationResult();
+            }            
+            
+        }
+        
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // TODO Auto-generated method stub
 
-		}
+        }
 
 
-		@Override
-		public void keyReleased(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-	
-	public Double getValue1() {
-		return Double.parseDouble(_value1.getText());
-	}
-	
-	public Double getValue2() {
-		return Double.parseDouble(_value2.getText());
-	}
-	
-	public Double getValue3() {
-		return Double.parseDouble(_value3.getText());
-	}
-	
-	public void clearFields() {
-		_value1.setText("");
-		_value2.setText("");
-		_value3.setText("");
-	}
-	
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+        
+    }
+    
+    public Double getValue1() {
+        return Double.parseDouble(_value1.getText());
+    }
+    
+    public Double getValue2() {
+        return Double.parseDouble(_value2.getText());
+    }
+    
+    public Double getValue3() {
+        return Double.parseDouble(_value3.getText());
+    }
+    
+    public void clearFields() {
+        _value1.setText("");
+        _value2.setText("");
+        _value3.setText("");
+    }
+    
 }
