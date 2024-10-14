fc8e517393af8ca61c913d634cbf91a85d13b2bd
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-14 19:05:29 -0300

    feat: dont receved more than one .

--- src/components/ui/NumericTextField.java
+++ src/components/ui/NumericTextField.java
@@ -3,21 +3,24 @@ package components.ui;
 import java.awt.Insets;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import javax.swing.JTextField;
 
 public class NumericTextField extends JTextField {
-	
+    
     public NumericTextField() {
-    	
-    	setMargin(new Insets(0, 10, 0, 0));
-    	
+        
+        setMargin(new Insets(0, 10, 0, 0));
+        
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 char c = e.getKeyChar();
                 
                 if (!Character.isDigit(c) && c != KeyEvent.VK_BACK_SPACE && c != '.') {
+                    e.consume();
+                }
+                
+                if (c == '.' && getText().contains(".")) {
                     e.consume();
                 }
             }
